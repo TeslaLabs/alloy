29b013c13339818a8423239fc53590881f2fbcdf
Blake Lucas <blake.c.lucas@gmail.com>
2017-01-13 13:50:54 -0800

    minor

--- include/core/AlloyOptimizationMath.h
+++ include/core/AlloyOptimizationMath.h
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2015, Blake C. Lucas, Ph.D. (img.science@gmail.com)
+ * Copyright(C) 2016, Blake C. Lucas, Ph.D. (img.science@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
